ffe146e4e1a9cdc108e158b0b16f0dc44de0640b
Nick Much <500160291@fvtc.edu>
2025-08-12 15:42:20 -0500

    Initial commit

---
+++ src/main/java/org/codex/codexeconomy/CodexEconomy.java
@@ -0,0 +1,16 @@
+package org.codex.codexeconomy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CodexEconomy implements ModInitializer {
+	public static final String MOD_ID = "codexeconomy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/org/codex/codexeconomy/CodexEconomyClient.java
@@ -0,0 +1,10 @@
+package org.codex.codexeconomy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CodexEconomyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
